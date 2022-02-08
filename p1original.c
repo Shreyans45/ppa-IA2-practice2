9abcfdde0d6e31a51fe00ebe2b472187b86604fa
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2022-02-08 06:45:55 +0000

    nothin'

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
-
+#include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("Enter the value of base: ");
+  scanf("%f",base);
+  printf("Enter the value of height :");
+  scanf("%f",height);
+}
+void find_area(float base,float height,float *area)
+{
+  *area=0.5*base*height;
+}
+void output(float base,float height,float area)
+{
+  printf("area  %f %f %f ",base,height,area);
+}
+int main()
+{
+  float base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
---
+++ p2original.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the value");
+  scanf("%d ",&n);
+  return n;
+}
+int check_scalene
